265370a8a1e1473d54b8ef3d47fe3be6ed83fa5f
Isaac Robinson <69752141+IJRCreate@users.noreply.github.com>
2025-06-22 19:57:11 +0100

    Updated README, added TODO items, added ability to add threat intel URLs

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # YASM
 A browser extension which allows users to query IOCs against threat intelligence platforms and configure which platforms they prefer to use.
+
+## 
+- [ ] Add tests
+- [ ] Add docs
+- [ ] Add context menus to search for IOCs on web pages
+- [ ] Add persistence to added platforms
+- [ ] Add some default platforms
+- [ ] Let users remove default platforms
+- [ ] Let users readd default platforms
+- [ ] Let users create groups for platforms
+- [ ] Allow users to search all platforms in a group
+- [ ] Make use of Toast notifications
+- [ ] Use Tailwind
+- [ ] Add a background image to the extension popup page
--- chromium/YASM-Chromium/src/routes/+page.svelte
+++ chromium/YASM-Chromium/src/routes/+page.svelte
@@ -1,3 +1,43 @@
 <script lang="ts">
 
+    let threatIntelligencePlatforms: URL[] = $state([]);
+
+    function addthreatIntelligencePlatform(event: SubmitEvent){
+        event.preventDefault()
+        const formData = new FormData(event.target as HTMLFormElement)
+        const formInput: string | undefined = formData.get("URL")?.toString()
+        if (formInput){
+            try{
+                const platformURL: URL = new URL(formInput)
+                threatIntelligencePlatforms.push(platformURL)
+            }catch(error){
+                console.error(error);
+                if (error instanceof Error) {
+                    alert(`Unable to add platform "${formInput}". ${error.message}. Is this a valid URL?`); // TODO Make this a Toast notification.
+                    } else {
+                    alert(`Unable to add platform "${formInput}. Is this a valid URL?`); // TODO Make this a Toast notification.
+                }
+            }
+        }
+    }
+
 </script>
+
+<form onsubmit={addthreatIntelligencePlatform}>
+	<label>
+		Add a new threat intelligence URL to query:
+		<input
+            type="text"
+			name="URL"
+			autocomplete="off"
+		/>
+	</label>
+    <button type="submit"> Add </button>
+</form>
+
+
+{#if threatIntelligencePlatforms}
+    {#each threatIntelligencePlatforms as threatIntelligencePlatform}
+        <p>{threatIntelligencePlatform.href}</p>
+    {/each}
+{/if}
